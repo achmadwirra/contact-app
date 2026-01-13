49f86fe65fcc2a43999418af14a5008338068538
achwir <achmad.wirra@gmail.com>
2026-01-13 09:26:25 +0800

    fix validation bug

--- contacts.js
+++ contacts.js
@@ -1,12 +1,10 @@
 const { rejects } = require('node:assert');
 const { resolve } = require('node:dns');
 const fs = require('node:fs');
-const readline = require('node:readline');
+const chalk = require('chalk');
+const validator = require('validator');
 
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-});
+
 const dirPath = './data';
 if (!fs.existsSync(dirPath)) {
     fs.mkdirSync(dirPath);
@@ -17,19 +15,32 @@ if (!fs.existsSync(dataPath)) {
     fs.writeFileSync(dataPath, '[]', 'utf-8');
 }
 
-const tulisPertanyaan = (pertanyaan) => {
-    return new Promise((resolve, reject) => {
-        rl.question(pertanyaan, (nama) => {
-            resolve(nama);
-        });
-    });
-};
-
 const simpanContact = (nama, email, noHP) => {
     const contact = { nama, email, noHP };
 
         const file = fs.readFileSync('data/contacts.json', 'utf-8');
         const contacts = JSON.parse(file);
+
+        // cek duplikat
+        const duplikat = contacts.find((contact) => contact.nama === nama);
+        if(duplikat) {
+            console.log(chalk.red.inverse.bold('contact sudah terdaftar, gunakan nama lain!'));
+            return false;
+        }
+
+        //cek email
+        if(email) {
+            if(!validator.isEmail(email)) {
+                console.log(chalk.red.inverse.bold('Email tidak valid!'));
+                return false;
+            }
+        //cek email
+            if(!validator.isMobilePhone(noHP, 'id-ID')) {
+                console.log(chalk.red.inverse.bold('No HP tidak valid!'));
+                return false;
+            }
+
+        }
 
         contacts.push(contact);
 
@@ -38,8 +49,9 @@ const simpanContact = (nama, email, noHP) => {
             JSON.stringify(contacts, null, 2)
         );
 
-        console.log('Terimakasih');
-        rl.close();
+        console.log(chalk.green.inverse.bold('Terimakasih sudah memasukkan data'));
+        
+        
 };
 
-module.exports = { tulisPertanyaan, simpanContact };
+module.exports = { simpanContact };
